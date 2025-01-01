73754526f99052c0e34cc8c862295e3fd80e9311
13073398 <mosamirsultan1985@gmail.com>
2025-01-01 20:50:48 +0200

    The end

--- java/master.js
+++ java/master.js
@@ -1,4 +1,4 @@
-// Dom Elements
+// to Dom Elements
 let min_menue_button=document.querySelector(".min-menue .min");
 let side_menue=document.querySelector(".side_menue");
 let side_menue_close=document.querySelector(".side_menue .heading .close");
@@ -11,10 +11,13 @@ let line_iso_menue=document.querySelectorAll(".line_iso .line_menue");
 let side_menue_category_open=document.querySelector(".min-menue .Categories");
 let side_menue_category_close_button=document.querySelector(".side_menue_category .heading .close");
 let  side_menue_category_menue=document.querySelector(".side_menue_category");
+let  side_menue_category_menue_line_under_tool=document.querySelectorAll(" .side_menue_category .side_menue_details .line_title");
 
+let popUp_Messge_box=document.querySelector(".bob_message");// popUp Message Dom
+let popUp_Messge_box_close=document.querySelector(".bob_message .text .close")//popUp Message Dom  close button
 
-
-
+let flash_message_box =document.querySelector(".flash_message");// flash message Dom
+let flash_message_box_close=document.querySelector(" .flash_message .right .line button");//flash message_close_button Dom
 
 
 //function to open Side meue //side_menue_category
@@ -40,12 +43,25 @@ side_menue_close.onclick=function(){
     line_menue_open.forEach((ele)=>{
         ele.innerHTML="+"
     })
-    
+
 }
 side_menue_category_close_button.onclick=function(){
     if(side_menue_category_menue.classList.contains("open")){
         side_menue_category_menue.classList.remove("open");
         over_lay.classList.remove("open");
+        line_menue.forEach((ele)=>{
+            ele.classList.remove("open")
+        })
+        line.forEach((ele)=>{
+            ele.classList.remove("expand")
+        })
+        line_menue_open.forEach((ele)=>{
+            ele.innerHTML="+"
+        })
+        
+        side_menue_category_menue_line_under_tool.forEach((ele)=>{
+            ele.classList.remove("on")
+        })
     }
 }
 //function to open/close  line menue 
@@ -64,9 +80,18 @@ line_menue_open.forEach((ele)=>{
         line.forEach((ele)=>{
             ele.classList.remove("expand")
         })
+        side_menue_category_menue_line_under_tool.forEach((ele)=>{
+            ele.classList.remove("on")
+        })
         line_menue.forEach((ele)=>{
             if(ele.dataset.name===e.currentTarget.dataset.name){
              ele.classList.add("open")
+             //to  draw  line under heading in category menue
+             side_menue_category_menue_line_under_tool.forEach((ele)=>{
+                if(ele.dataset.name===e.currentTarget.dataset.name){
+                    ele.classList.add("on")
+                }
+             })
              
             }
          })
@@ -76,11 +101,15 @@ line_menue_open.forEach((ele)=>{
              }
          })
          e.currentTarget.innerHTML="-"
-    }else if(e.currentTarget.innerHTML==="-"){
+        }else if(e.currentTarget.innerHTML==="-"){
         line_menue.forEach((ele)=>{
             if(ele.dataset.name===e.currentTarget.dataset.name){
              ele.classList.remove("open")
-             
+             side_menue_category_menue_line_under_tool.forEach((ele)=>{
+                if(ele.dataset.name===e.currentTarget.dataset.name){
+                    ele.classList.remove("on")
+                }
+             })
             }
          })
          line.forEach((ele)=>{
@@ -105,12 +134,14 @@ line_iso_open_sighn.forEach((ele)=>{
 line_iso_open_sighn.forEach((ele)=>{
     ele.addEventListener("click",(e)=>{
         line_iso_menue.forEach((ele)=>{
-          
             if(ele.dataset.name===e.currentTarget.dataset.name){
                 if(ele.classList.contains("open")){
                     ele.classList.remove("open")
                 }else if(ele.classList.contains("open")===false && e.currentTarget.classList.contains("rotate")===false){
                     ele.classList.add("open")
+                    line_iso_open_sighn.forEach((ele)=>{
+                        ele.classList.remove("rotate")
+                    })
                    
                 }
                
@@ -129,6 +160,33 @@ line_iso_open_sighn.forEach((ele)=>{
     })
 })
 
+ // function to make popUp Message
+function popUp_message(){
+    popUp_Messge_box.classList.add("open");
+    over_lay.classList.add("bob")
+}
+setTimeout(popUp_message,10000)
+// function to close popUp Message
+popUp_Messge_box_close.onclick=function(){
+    popUp_Messge_box.remove();
+    over_lay.classList.remove("bob")
+}
+//function to make flash message
+function flash_messge(){
+    flash_message_box.classList.add("open"); 
+   
+   
+}
+setInterval(flash_messge,1000)
+setInterval(flash_messge_cancel,10000)
+function flash_messge_cancel(){
+    flash_message_box.classList.remove("open");  
+}
+
+//function to close flash message box 
+flash_message_box_close.onclick=function(){
+    flash_message_box.remove()
+}
 
 
 
